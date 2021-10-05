652c57dc7eda5221d45649469895ddbab632ab58
nishanmazumder <arosh019@gmail.com>
2021-10-05 22:13:15 +0600

    add candidate data

--- candidate.php
+++ candidate.php
@@ -8,11 +8,12 @@ $db = new Connect;
 //Get message info
 $input = file_get_contents("php://input");
 $data = json_decode($input);
+$user_name =  $data->message->from->first_name;
 $chat_id = $data->message->chat->id;
 $text = $data->message->text;
 
 if ($text == "/start") {
-    $msg = "Welcome! please enter your roll number:";
+    $msg = "Welcome $user_name! please enter your roll number:";
 } else {
     $text = mysqli_real_escape_string($db->con, $text);
 
@@ -27,13 +28,16 @@ if ($text == "/start") {
         $result = mysqli_query($db->con, $query);
 
         $marks = "";
+        $total = 0;
 
         while ($row = mysqli_fetch_assoc($result)) {
             $marks .= $row['subject'] . " : " . $row['mark'] ."%". "%0A";
+            $total = $total + $row['mark'] ."%". "%0A";
         }
 
         $msg = "<b>Name : $name</b>%0A";
         $msg .= $marks;
+        $msg .= "<b>Total : $total</b>";
     } else {
         $msg = "Please enter you valid roll number!";
     }
