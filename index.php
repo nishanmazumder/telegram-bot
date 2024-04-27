4bdc734a1a4d938224f5b0616c8fe5bf5991a3aa
Md Rashidul Azam (Nishan) <arosh019@gmail.com>
2024-04-27 11:46:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
 include_once './token.php';
 
-// Set Web hook info
-// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/setWebhook?url=https://telebot.bdsoftcreation.com/index.php
-
-// Get Web Hook info
-// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/getWebhookInfo
-
-// Delete Webhook info
-// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/deleteWebhook
-
-// CURL
-// $ch = curl_init();
-
-// $url = "http://telebot.bdsoftcreation.com/index.php";
-
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_TIMEOUT, 3000);
-
-// $arr = ["name" => "NISHAN"];
-
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $arr);
-
-// $result = curl_exec($ch);
-
-// print_r($result);
-
-// curl_close($ch);
-
 if(isset($_POST['submit'])){
 
 $token = TOKEN_CANDIDATE;
@@ -43,17 +15,20 @@ curl_setopt($ch, CURLOPT_URL, $send_msg);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 $result = json_decode($result, true);
-
-// echo '<pre>'; 
-// print_r($result);
-
 curl_close($ch);
-
 }
 ?>
-
-
+	
 <form action="" method="post">
 	<input type="text" name="msg" id="">
 	<input type="submit" name="submit" value="Send">
 </form>
+
+// Set Web hook info
+// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/setWebhook?url=https://telebot.bdsoftcreation.com/index.php
+
+// Get Web Hook info
+// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/getWebhookInfo
+
+// Delete Webhook info
+// https://api.telegram.org/bot2026548842:AAE52wMvxNWqqFPpA9OnyJKo2YVnBxiYvm0/deleteWebhook
